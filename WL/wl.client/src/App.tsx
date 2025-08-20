5136b0c85cf8bcb871a1afbfaf4cd54c8d156f4d
landynakiri <will63004@gmail.com>
2025-08-20 18:10:46 +0800

    [OOXX] add onClick

--- WL/wl.client/src/App.tsx
+++ WL/wl.client/src/App.tsx
@@ -55,8 +55,19 @@ function App() {
     }
 }
 
-function Square({ value }: { value: string} ) {
-    return <button className="square">{value}</button>;
+function Square({ value }: { value: string }) {
+    function handleClick() {
+        console.log('clicked!');
+    }
+
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
 }
 
 export default function Board() {
