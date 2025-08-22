d1b086239ad9ef1c5ac71134e995c7814b15fe37
landynakiri <will63004@gmail.com>
2025-08-22 16:36:50 +0800

    新增OX回合轉換

--- WL/wl.client/src/App.tsx
+++ WL/wl.client/src/App.tsx
@@ -67,12 +67,20 @@ function Square({ value, onSquareClick }: { value : string | null , onSquareClic
 }
 
 export default function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState<(string | null)[]>(Array(9).fill(null));
 
     function handleClick(i: number) {
         const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        }
+        else {
+            nextSquares[i] = 'O';
+        }
+        
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
 
     return (
