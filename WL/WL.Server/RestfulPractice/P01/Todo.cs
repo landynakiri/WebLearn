b7a6a1cd65d7dd0b21d51ffe50941cac57c53579
landynakiri <will63004@gmail.com>
2025-09-05 17:25:11 +0800

    測試各類Restful

---
+++ WL/WL.Server/RestfulPractice/P01/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace WL.Server.RestfulPractice.P01
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ WL/WL.Server/RestfulPractice/P01/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WL.Server.RestfulPractice.P01
+{
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options)
+        {
+        }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
