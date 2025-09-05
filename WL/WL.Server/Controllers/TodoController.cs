faa0c09bf1334e71d14f1193b5079617b88a2cde
landynakiri <will63004@gmail.com>
2025-09-05 18:35:15 +0800

    使用controller建立API (待續)

---
+++ WL/WL.Server/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WL.Server.Model;
+
+namespace WL.Server.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController: ControllerBase
+    {
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+    }
+}
