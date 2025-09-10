7d9ce2aa31d3824d700d3031f952e505c4651881
landynakiri <will63004@gmail.com>
2025-09-10 17:28:01 +0800

    新增identity認證

--- Bank/Bank.Server/Program.cs
+++ Bank/Bank.Server/Program.cs
@@ -1,4 +1,10 @@
 
+using Bank.Server.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace Bank.Server
 {
     public class Program
@@ -8,6 +14,12 @@ namespace Bank.Server
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            // -------------------- Identity 設定區塊 --------------------
+            builder.Services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseInMemoryDatabase("AppDb"));
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddAuthorization();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +27,21 @@ namespace Bank.Server
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            // -------------------- Identity 路由 --------------------
+            app.MapIdentityApi<IdentityUser>();
+            app.MapPost("/logout", async (SignInManager<IdentityUser> signInManager,
+                [FromBody] object empty) =>
+                {
+                    if (empty != null)
+                    {
+                        await signInManager.SignOutAsync();
+                        return Results.Ok();
+                    }
+                    return Results.Unauthorized();
+                })
+                .WithOpenApi()
+                .RequireAuthorization();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
