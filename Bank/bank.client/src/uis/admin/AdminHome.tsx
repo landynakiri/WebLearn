7abf57cc69bd37b0b99f540121cf00badd0b9144
landynakiri <will63004@gmail.com>
2025-09-17 17:16:50 +0800

    新增AdminHome

---
+++ Bank/bank.client/src/uis/admin/AdminHome.tsx
@@ -0,0 +1,8 @@
+export default function AdminHome() {
+    return (
+        <div>
+            <h1>Admin Home</h1>
+            <p>Welcome, Admin! You can manage user accounts here.</p>
+        </div>
+    );
+}
