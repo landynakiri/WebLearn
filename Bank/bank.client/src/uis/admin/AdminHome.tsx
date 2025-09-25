36a6cd957d00b0bfcc5d9f9a8f7feb5575e78e44
landynakiri <will63004@gmail.com>
2025-09-25 14:50:18 +0800

    顯示使用者列表

--- Bank/bank.client/src/uis/admin/AdminHome.tsx
+++ Bank/bank.client/src/uis/admin/AdminHome.tsx
@@ -2,16 +2,10 @@ import { getUserApi } from '../../services/openApiGeneratorServices'
 import { useEffect, useState } from 'react';
 import type { GetUserResp } from '../../generated/models'
 export default function AdminHome() {
-    async function GetUsers() {
-        const result = await getUserApi().usersGetUsers()  
-        alert(result)
-    }
-
     return (
         <div>
             <h1>Admin Home</h1>
             <p>Welcome, Admin! You can manage user accounts here.</p>
-            <button onClick={() => GetUsers()}>GetUsersTestBtn</button>
             <UserTable></UserTable>
         </div>
     );
@@ -33,10 +27,31 @@ function UserTable() {
     }, []);
 
     return (
-        <ul>
-            {users.map((u: GetUserResp, i) => (
-                <li key={i}>{u.id}, {u.userName}, {u.email}, {u.roles}, {u.createdAt?.toDateString()}, { u.lastLogin?.toDateString()}</li>
-            ))}
-        </ul>
+        <table border={1} cellPadding={5}>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>UserName</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>CreateAt</th>
+                    <th>LastLogin</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map(a => (
+                    <tr key={a.id}>
+                        <td>{a.id}</td>
+                        <td>{a.userName}</td>
+                        <td>{a.email}</td>
+                        <td>
+                            {a.roles?.join(", ")}
+                        </td>
+                        <td>{a.createdAt?.toDateString()}</td>
+                        <td>{a.lastLogin?.toDateString()}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
